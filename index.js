1a1c1b02d957e8b03a8f803191b098a89077c557
csantilucas <lucascsanti2006@gmail.com>
2025-09-29 16:00:50 -0400

    crateRoutes_api

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const post = require('./model/post')
+const apiRoute = require('./routes/api')
 const app = express();
 const port = 3000;
+const path = require('path')
 
 
 
+app.use('/api',apiRoute)
+app.use (express.static(path.join(__dirname, "public")));
 
-app.get('/get', (req, res) => {
 
-    res.json(JSON.stringify(post.getAll()))
-
-})
-
-app.post('/post',bodyParser.json(), (req, res) => {
-    
-    let title = req.body.title;
-    let description = req.body.description;
-    post.newPost(title, description)    
-    res.send('Post created')
-})
-
-app.delete('/delete/:id',(res, req) => {
-    let id = req.params.id
-    post.deletePost(id)
-    res.send('post deletado com sucesso')
-} )
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
---
+++ routes/api.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router()
+const bodyParser = require('body-parser');
+const post = require('../model/post')
+
+router.get('/all', (req, res) => {
+
+    res.json(JSON.stringify(post.getAll()))
+
+})
+
+router.post('/new',bodyParser.json(), (req, res) => {
+    
+    let title = req.body.title;
+    let description = req.body.description;
+    post.newPost(title, description)    
+    res.send('Post created')
+})
+
+router.delete('/delete/:id',(res, req) => {
+    let id = req.params.id
+    post.deletePost(id)
+    res.send('post deletado com sucesso')
+} )
+
+
+module.exports=router
